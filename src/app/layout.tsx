eabeed06341951f75895ce49b2ff4a72cf0a11f5
bnhtradingprojects-oss <bnhtradingprojects-oss.github.io/>
2025-08-25 18:02:19 +0200

    added '@vercel/analytics/next' by AEN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
+import { Analytics } from '@vercel/analytics/next';
 const geist = Geist({
   subsets: ["latin"],
 });
@@ -21,6 +21,7 @@ export default function RootLayout({
       <body className={`${geist.className} antialiased`}>
         <Navbar />
         <div>{children}</div>
+         <Analytics />
       </body>
     </html>
   );
